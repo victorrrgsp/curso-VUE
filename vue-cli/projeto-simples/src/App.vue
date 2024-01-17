d3af55c9b76f1c4a11653d9eb64a606cdf961983
Victorgsp <victorgabrielpimenta52@gmail.com>
2024-01-17 15:33:41 -0300

    criando projeto simples

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        {{ titulo }}
+        <button @click="titulo += '#'">Aumentar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            titulo: 'Teste Data usando Vue js!!'
+        }
+    }
+}
+</script>
+
+<style>
+    #app{
+        background-color: chocolate;
+        color: #fff;
+    }
+</style>
